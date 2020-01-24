366921e0865085b1b5b5dca8a27541471a8c38ee
Bernhard B <schluchti@gmail.com>
2020-01-24 20:35:24 +0100

    adapted api to stay backwards compatible

--- pysignalclirestapi/api.py
+++ pysignalclirestapi/api.py
@@ -10,20 +10,12 @@ class SignalCliRestApiError(Exception):
 
 class SignalCliRestApi(object):
     """SignalCliRestApi implementation."""
-    def __init__(self, base_url, number, api_version=1):
+    def __init__(self, base_url, number):
         """Initialize the class."""
         super(SignalCliRestApi, self).__init__()
         
-        try:
-            self._api_version = int(api_version)
-        except ValueError:
-            raise SignalCliRestApiError("api version needs to be an integer!")
-        
         self._base_url = base_url
         self._number = number
-
-        if self._api_version > 2:
-            raise SignalCliRestApiError("api version not supported!")
 
     def supported_api_versions(self):
         try:
@@ -39,25 +31,36 @@ class SignalCliRestApi(object):
          
         Additionally a file can be attached.
         """
-        url = self._base_url + "/v" + str(self._api_version) + "/send"
+        
+        api_versions = self.supported_api_versions()
+        if filenames is not None and len(filenames) > 1:
+            if "v2" not in api_versions: # multiple attachments only allowed when api version >= v2
+                raise SignalCliRestApiError("This signal-cli-rest-api version is not capable of sending multiple attachments. Please upgrade your signal-cli-rest-api docker container!")
+        
+        
+        url = self._base_url + "/v2/send"
+        if "v2" not in api_versions: # fall back to old api version to stay downwards compatible.
+            url = self._base_url + "/v1/send"
+
         data = {
             "message": message,
             "number": self._number,
             "recipients": recipients,
-        }
-
-        api_versions = self.supported_api_versions()
-        if filenames is not None and len(filenames) > 1:
-            if "v2" not in api_versions:
-                raise SignalCliRestApiError("This signal-cli-rest-api version is not capable of sending multiple attachments. Please upgrade your signal-cli-rest-api docker container!")
+        } 
 
         try:
-            base64_attachments = []
-            if filenames is not None: 
-                for filename in filenames:
-                    with open(filename, "rb") as ofile:
-                        base64_attachments.append(str(base64.b64encode(ofile.read()), "utf-8"))
-            data["base64_attachments"] = base64_attachments
+            if "v2" in api_versions:
+                base64_attachments = []
+                if filenames is not None: 
+                    for filename in filenames:
+                        with open(filename, "rb") as ofile:
+                            base64_attachments.append(str(base64.b64encode(ofile.read()), "utf-8"))
+                data["base64_attachments"] = base64_attachments
+            else: # fall back to api version 1 to stay downwards compatible
+                if filenames is not None and len(filenames) == 1:
+                    with open(filenames[0], "rb") as ofile:
+                        data["base64_attachment"] = str(base64.b64encode(ofile.read()), "utf-8")
+            
             resp = requests.post(url, json=data)
             if resp.status_code != 201:
                 json_resp = resp.json()
